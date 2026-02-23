2bb90a3b43a755c8be813b97840aa73806c7dfc2
samridhij18 <sj5922@srmist.edu.in>
2026-02-23 09:24:15 +0530

    Added-UC3

---
+++ UC4.java
@@ -0,0 +1,19 @@
+public class UC4 {
+    public static void main(String args[]) {
+
+        String[] banner = {
+            "  ***      ***    ******    ******  ",
+            " ** **    ** **   **    **  **      ",
+            "**   **  **   **  **    **  **      ",
+            "**   **  **   **  ******     ****** ",
+            "**   **  **   **  **              **",
+            "**   **  **   **  **              **",
+            " ** **    ** **   **        **    **",
+            "  ***      ***    **         ****** "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
